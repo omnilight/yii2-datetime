226dead40bccc6c8b497548b0288ad9f837b5665
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-09-18 14:23:47 +0400

    Added DateTime managing function

---
+++ DateTimeAttribute.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace omnilight\datetime;
+
+use yii\base\Object;
+
+
+/**
+ * Class DateTimeAttribute
+ * @property string $value
+ */
+class DateTimeAttribute extends Object
+{
+    /**
+     * @var DateTimeBehavior
+     */
+    public $behavior;
+    /**
+     * @var string
+     */
+    public $originalAttribute;
+    /**
+     * @var string|array
+     */
+    public $originalFormat;
+    /**
+     * @var string|array
+     */
+    public $targetFormat;
+    /**
+     * @var string
+     */
+    protected $_value;
+
+    function __toString()
+    {
+        return $this->getValue();
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        if ($this->_value)
+            return $this->_value;
+        else
+            return $this->behavior->formatter->format($this->behavior->owner->{$this->originalAttribute}, $this->targetFormat);
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->_value = $value;
+        $this->behavior->owner->{$this->originalAttribute} = $this->behavior->formatter->format($this->_value, $this->originalFormat);
+    }
+}
--- DateTimeBehavior.php
+++ DateTimeBehavior.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace omnilight\datetime;
+
 use yii\base\Behavior;
+use yii\helpers\ArrayHelper;
+use yii\i18n\Formatter;
 
 
 /**
@@ -9,5 +12,135 @@ use yii\base\Behavior;
  */
 class DateTimeBehavior extends Behavior
 {
+    /**
+     * @var string
+     */
+    public $namingTemplate = '{attribute}_local';
+    /**
+     * @var Formatter
+     */
+    public $formatter;
+    /**
+     * @var string|array
+     */
+    public $originalFormat = 'datetime';
+    /**
+     * @var string|array
+     */
+    public $targetFormat = 'datetime';
+    /**
+     * @var array
+     */
+    public $attributes = [];
+    /**
+     * @var array
+     */
+    public $attributeConfig = ['class' => 'omnilight\datetime\DateTimeAttribute'];
+    /**
+     * @var bool
+     */
+    public $performValidation = true;
+    /**
+     * @var DateTimeAttribute[]
+     */
+    public $attributeValues = [];
+
+    public function init()
+    {
+        if (is_null($this->formatter))
+            $this->formatter = \Yii::$app->formatter;
+        elseif (is_array($this->formatter))
+            $this->formatter = \Yii::createObject($this->formatter);
+
+        $this->prepareAttributes();
+    }
+
+    protected function prepareAttributes()
+    {
+        foreach ($this->attributes as $key => $value) {
+            $config = $this->attributeConfig;
+            $config['originalFormat'] = $this->originalFormat;
+            $config['targetFormat'] = $this->targetFormat;
+            if (is_integer($key)) {
+                $originalAttribute = $value;
+                $targetAttribute = $this->processTemplate($originalAttribute);
+            } else {
+                $originalAttribute = $key;
+                if (is_string($value)) {
+                    $targetAttribute = $value;
+                } else {
+                    $targetAttribute = ArrayHelper::remove($value, 'targetAttribute', $this->processTemplate($originalAttribute));
+                    $config = array_merge($config, $value);
+                }
+            }
+            $config['behavior'] = $this;
+            $config['originalAttribute'] = $originalAttribute;
+
+            $this->attributeValues[$targetAttribute] = $config;
+        }
+    }
+
+    protected function processTemplate($originalAttribute)
+    {
+        return strtr($this->namingTemplate, [
+            '{attribute}' => $originalAttribute,
+        ]);
+    }
+
+    public function canGetProperty($name, $checkVars = true)
+    {
+        if ($this->hasAttributeValue($name))
+            return true;
+        else
+            return parent::canGetProperty($name, $checkVars);
+    }
+
+    protected function hasAttributeValue($name)
+    {
+        return isset($this->attributeValues[$name]);
+    }
+
+    public function canSetProperty($name, $checkVars = true)
+    {
+        if ($this->hasAttributeValue($name))
+            return true;
+        else
+            return parent::canSetProperty($name, $checkVars);
+    }
+
+    public function __get($name)
+    {
+        if ($this->hasAttributeValue($name))
+            return $this->getAttributeValue($name);
+        return parent::__get($name);
+    }
+
+    public function __set($name, $value)
+    {
+        if ($this->hasAttributeValue($name))
+            $this->setAttributeValue($name, $value);
+        parent::__set($name, $value);
+    }
+
+    protected function getAttributeValue($name)
+    {
+        if (is_array($this->attributeValues[$name])) {
+            $this->attributeValues[$name] = \Yii::createObject($this->attributeValues[$name]);
+        }
+        return $this->attributeValues[$name];
+    }
+
+    protected function setAttributeValue($name, $value)
+    {
+        if (is_array($this->attributeValues[$name])) {
+            $this->attributeValues[$name] = \Yii::createObject($this->attributeValues[$name]);
+        }
+
+        if ($value instanceof DateTimeAttribute)
+            $this->attributeValues[$name] = $value;
+        else
+            $this->attributeValues[$name]->value = $value;
+    }
+
 
 } 
